7d3abd16a5fe80991af252dc6b3630b30c741b38
Hemant Kumar <hemant.k2807@gmail.com>
2025-08-24 10:29:54 +0530

    Inngest integration for Background flows

--- app/provider.js
+++ app/provider.js
@@ -2,6 +2,7 @@
 import { db } from "@/configs/db";
 import { USER_TABLE } from "@/configs/schema";
 import { useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { eq } from "drizzle-orm";
 import React, { useEffect } from "react";
 
@@ -15,26 +16,30 @@ function Provider({ children }) {
   const CheckIsNewUser = async () => {
     //Check if New User Exists
 
-    const result = await db
-      .select()
-      .from(USER_TABLE)
-      .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+    // const result = await db
+    //   .select()
+    //   .from(USER_TABLE)
+    //   .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
 
-    console.log(result);
+    // console.log(result);
 
-    if (result?.length == 0) {
-      //Else add to DB
-      const userResp = await db
-        .insert(USER_TABLE)
-        .values({
-          name: user?.fullName,
-          email: user?.primaryEmailAddress?.emailAddress,
-        })
-        .returning({ id: USER_TABLE.id });
+    // if (result?.length == 0) {
+    //   //Else add to DB
+    //   const userResp = await db
+    //     .insert(USER_TABLE)
+    //     .values({
+    //       name: user?.fullName,
+    //       email: user?.primaryEmailAddress?.emailAddress,
+    //     })
+    //     .returning({ id: USER_TABLE.id });
 
-      console.log(userResp);
-    }
-  };
+    //   console.log(userResp);
+
+    //}
+
+    const resp= await axios.post('/api/create-user', {user:user});
+    console.log(resp.data);
+  }
 
   return <div>{children}</div>;
 }
--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials:{
-    url:'postgresql://neondb_owner:npg_FmrZH5evgX8s@ep-noisy-pine-aecp0dgg-pooler.c-2.us-east-2.aws.neon.tech/aurevo?sslmode=require&channel_binding=require'
+    url:"postgresql://neondb_owner:npg_LIjFTz3KZau5@ep-noisy-pine-aecp0dgg-pooler.c-2.us-east-2.aws.neon.tech/aurevo?sslmode=require&channel_binding=require"
   }
 });
